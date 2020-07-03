b4a2c134201f56a4ef03f6b346826e5589f40f45
Nguyen Hien <44672087+nguyenthanhhienitnlu@users.noreply.github.com>
2020-07-03 21:14:25 +0700

    Create Class SendMail to send notification when changing.
    Create Class SendMail to send notification when changing.

---
+++ SendMail.java
@@ -0,0 +1,45 @@
+package notification;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+	public static boolean sendMail(String to, String subject, String text) {
+		Properties props = new Properties();
+		props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+			@Override
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication("shoeshop.nlu@gmail.com", "shoeshop77");
+			}
+		});
+		try {
+			Message message = new MimeMessage(session);
+			message.setHeader("Content-Type", "text/plain; charset=UTF-8");
+			message.setFrom(new InternetAddress("shoeshop.nlu@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+			message.setSubject(subject);
+			message.setText(text);
+			Transport.send(message);
+		} catch (MessagingException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
+		return true;
+
+	}
+	public static void main(String[] args) {
+		System.out.println(sendMail("nguyenthanhhien.itnlu@gmail.com", "hehe", "haha"));
+	}
+}
