fda5f9c58bf899b297bfbd0ee466389020be059b
17130068 <55530394+17130068@users.noreply.github.com>
2020-08-02 23:22:51 +0700

    Update PSCPProcess.java

--- PSCPProcess.java
+++ PSCPProcess.java
@@ -31,51 +31,5 @@ public class PSCPProcess {
 		return scp_download;
 	}
 	
-// 	public String selectField(int id_scp, String table, String field) throws SQLException{
-// 		String sql = "SELECT " + field + " FROM " +table + " WHERE id=?";
-// 		System.out.println(sql);
-// 		try {
-// 			ptmt=DBConnection.createConnection(controlDB.getSource_db()).prepareStatement(sql);
-// 			ptmt.setInt(1, id_scp);
-// 			rs=ptmt.executeQuery();
-// 			rs.next();
-// 			return rs.getString(field); 
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			return null;
-// 		}finally {
-// 			if(ptmt!=null){
-// 				ptmt.close();
-// 			}
-// 			if(rs!=null){
-// 				rs.close();
-// 			}
-// 		}
-		
-// 	}
-// 	public int selectPortField(int id_scp, String table, String field) throws SQLException{
-// 		String sql = "SELECT " + field + " FROM " +table + " WHERE id=?";
-// 		System.out.println(sql);
-// 		try {
-// 			ptmt=DBConnection.createConnection(controlDB.getSource_db()).prepareStatement(sql);
-// 			ptmt.setInt(1, id_scp);
-// 			rs=ptmt.executeQuery();
-// 			rs.next();
-// 			return rs.getInt(field); 
-// 		} catch (SQLException e) {
-// 			e.printStackTrace();
-// 			return -1;
-// 		}finally {
-// 			if(ptmt!=null){
-// 				ptmt.close();
-// 			}
-// 			if(rs!=null){
-// 				rs.close();
-// 			}
-// 		}
-		
-// 	}
-	
-	
 
 }
