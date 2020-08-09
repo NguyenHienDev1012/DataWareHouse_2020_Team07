56dc94f4abd182d25d59500119cbef35e9dd62a3
17130068 <55530394+17130068@users.noreply.github.com>
2020-08-09 19:46:53 +0700

    Update DownloadPSCP.java
    thêm phương thức cập nhật trạng thái trong log_file khi file đã download về

--- DownloadPSCP.java
+++ DownloadPSCP.java
@@ -30,6 +30,7 @@ public class DownloadPSCP {
 		CkSsh ssh = new CkSsh();
 		CkGlobal ck = new CkGlobal();
 		ck.UnlockBundle("H");
+		
 		String hostname;
 		hostname = scp_download.getHostname();
 		int port = scp_download.getPortconnect();
@@ -40,6 +41,7 @@ public class DownloadPSCP {
 			return;
 		}
 		ssh.put_IdleTimeoutMs(5000);
+		
 		String username = scp_download.getUsername();
 		String pass = scp_download.getPassword();
 		success = ssh.AuthenticatePw(username, pass);
@@ -72,7 +74,50 @@ public class DownloadPSCP {
 
 		ssh.Disconnect();
 	}
+// Phương thức này để cập nhật trạng thái trong log_file khi file đã download về.
+	public void loadFileStatus(DataProcess dp, String table_name, int id_config) throws SQLException{
+	       // Lấy thời gian hiện tại.
+	       	String timestamp= getCurrentTime();
+	       // Lấy tất cả dữ liệu các trường configuration theo id_config 
+			Configuration configuration=dp.getControlDb().selectAllFieldConfigurationByConfigId((id_config));
+			String import_dir = configuration.getImport_dir();
+			int config_id = configuration.getConfig_id();
+			//Lấy tất cả tên của các file đã ghi vào log_file
+			ArrayList<String> listFileNameCurrent=dp.getControlDb().selectAllFileNameInLogFile("log_file");
 
+			File imp_dir = new File(import_dir);
+			if (imp_dir.exists()) {
+				File[] listFile = imp_dir.listFiles();
+				if(listFile.length>0){
+				for(File f: listFile){
+					if(listFileNameCurrent.size()>0){
+						// Nếu danh sách tên file hiện tại trong log mà không chứa tên file ta muốn ghi log thì tiến hành ghi log.
+						if(!listFileNameCurrent.contains(f.getName())){
+							dp.getControlDb().insertLogFileStatus(table_name, f.getName(), config_id, FILE_STATUS_READY,timestamp);
+					}
+				}
+					else{
+						dp.getControlDb().insertLogFileStatus(table_name, f.getName(), config_id, FILE_STATUS_READY,timestamp);
+					}
+				}
+				
+				SendMail.sendMail(MailConfig.EMAIL_RECEIVER, "URGENT FILE INFORMATION",
+						"Load file status successfully!");
+				System.out.println("Load file status successfully!");
+			}
+				else{
+					SendMail.sendMail(MailConfig.EMAIL_RECEIVER, MailConfig.EMAIL_TITLE,
+							"No any file here to load file status!");
+					System.out.println("No any file here to load file status!");
+				}
+				
+			}
+			else{
+				System.out.println("Path not exists!!!");
+				return;
+			}
+			
+		}
 	//// pscp -r -P 2227
 	//// guest_access@drive.ecepvn.org:/volume1/ECEP/song.nguyen/DW_2020/data/
 	//// D:\\DataWareHouse\\AllFile
